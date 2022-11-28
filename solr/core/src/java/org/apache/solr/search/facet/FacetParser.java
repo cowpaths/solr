apache/lucene-solr

Add facet parser factories and plugin support. (#122)
Add facet parser factories and plugin support for adding custom
facet parsers and facet processors.

solr/core/src/java/org/apache/solr/search/facet/FacetParser.java
@@ -16,8 +16,10 @@
  */
 package org.apache.solr.search.facet;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -26,15 +28,15 @@ import org.apache.solr.common.SolrException;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.common.util.StrUtils;
-import org.apache.solr.search.FunctionQParser;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.schema.IndexSchema;
+import org.apache.solr.search.FunctionQParser;
 import org.apache.solr.search.QParser;
 import org.apache.solr.search.SyntaxError;
 
 import static org.apache.solr.common.params.CommonParams.SORT;
 
-abstract class FacetParser<FacetRequestT extends FacetRequest> {
+public abstract class FacetParser<FacetRequestT extends FacetRequest> {
   protected FacetRequestT facet;
   protected FacetParser<?> parent;
   protected String key;
@@ -59,7 +61,7 @@ abstract class FacetParser<FacetRequestT extends FacetRequest> {
     return new SolrException(SolrException.ErrorCode.BAD_REQUEST, msg + " , path="+getPathStr());
   }
 
-  public abstract FacetRequest parse(Object o) throws SyntaxError;
+  public abstract FacetRequestT parse(Object o) throws SyntaxError;
 
   // TODO: put the FacetRequest on the parser object?
   public void parseSubs(Object o) throws SyntaxError {
@@ -134,22 +136,16 @@ abstract class FacetParser<FacetRequestT extends FacetRequest> {
   }
 
   public Object parseFacetOrStat(String key, String type, Object args) throws SyntaxError {
-    // TODO: a place to register all these facet types?
-
-    switch (type) {
-      case "field":
-      case "terms":
-        return new FacetFieldParser(this, key).parse(args);
-      case "query":
-        return new FacetQueryParser(this, key).parse(args);
-      case "range":
-        return new FacetRangeParser(this, key).parse(args);
-      case "heatmap":
-        return new FacetHeatmap.Parser(this, key).parse(args);
-      case "func":
+    // First check facet parser factories.
+    FacetParserFactory facetParserFactory = getSolrRequest().getCore().getFacetParserFactory(type);
+    if (facetParserFactory != null) {
+      return facetParserFactory.create(this, key).parse(args);
+    }
+    // Then handle special case func directly.
+    if (type.equals("func")) {
         return parseStat(key, args);
     }
-
+    // Otherwise, unknown.
     throw err("Unknown facet or stat. key=" + key + " type=" + type + " args=" + args);
   }
 
@@ -488,6 +484,13 @@ abstract class FacetParser<FacetRequestT extends FacetRequest> {
 
       return facet;
     }
+    
+    public static class Factory implements FacetParserFactory {
+      @Override
+      public FacetParser<?> create(FacetParser<?> parent, String key) {
+        return new FacetQueryParser(parent, key);
+      }
+    }
   }
 
   /*** not a separate type of parser for now...
@@ -636,6 +639,23 @@ abstract class FacetParser<FacetRequestT extends FacetRequest> {
       return facetSort;
     }
 
+    public static class Factory implements FacetParserFactory {
+      @Override
+      public FacetParser<?> create(FacetParser<?> parent, String key) {
+        return new FacetFieldParser(parent, key);
+      }
+    }
   }
 
+  public static final Map<String, Class<? extends FacetParserFactory>> standard_factories;
+
+  static {
+    HashMap<String, Class<? extends FacetParserFactory>> map = new HashMap<>();
+    map.put("field", FacetParser.FacetFieldParser.Factory.class);
+    map.put("terms", FacetParser.FacetFieldParser.Factory.class);
+    map.put("query", FacetParser.FacetQueryParser.Factory.class);
+    map.put("range", FacetRangeParser.Factory.class);
+    map.put("heatmap", FacetHeatmap.Parser.Factory.class);
+    standard_factories = Collections.unmodifiableMap(map);
+  }
 }

solr/core/src/java/org/apache/solr/search/facet/FacetProcessor.java
@@ -46,16 +46,16 @@ import org.apache.solr.search.facet.SlotAcc.SlotContext;
 
 /** Base abstraction for a class that computes facets. This is fairly internal to the module. */
 public abstract class FacetProcessor<FacetRequestT extends FacetRequest>  {
-  SimpleOrderedMap<Object> response;
-  FacetContext fcontext;
-  FacetRequestT freq;
+  protected SimpleOrderedMap<Object> response;
+  protected FacetContext fcontext;
+  protected FacetRequestT freq;
 
   DocSet filter;  // additional filters specified by "filter"  // TODO: do these need to be on the context to support recomputing during multi-select?
   LinkedHashMap<String,SlotAcc> accMap;
   SlotAcc[] accs;
-  SlotAcc.CountSlotAcc countAcc;
+  protected SlotAcc.CountSlotAcc countAcc;
 
-  FacetProcessor(FacetContext fcontext, FacetRequestT freq) {
+  public FacetProcessor(FacetContext fcontext, FacetRequestT freq) {
     this.fcontext = fcontext;
     this.freq = freq;
     fcontext.processor = this;
@@ -333,7 +333,7 @@ public abstract class FacetProcessor<FacetRequestT extends FacetRequest>  {
     }
   }
 
-  int collect(DocSet docs, int slot, IntFunction<SlotContext> slotContext) throws IOException {
+  protected int collect(DocSet docs, int slot, IntFunction<SlotContext> slotContext) throws IOException {
     int count = 0;
     SolrIndexSearcher searcher = fcontext.searcher;
 
@@ -392,7 +392,7 @@ public abstract class FacetProcessor<FacetRequestT extends FacetRequest>  {
     }
   }
 
-  void addStats(SimpleOrderedMap<Object> target, int slotNum) throws IOException {
+  protected void addStats(SimpleOrderedMap<Object> target, int slotNum) throws IOException {
     int count = countAcc.getCount(slotNum);
     target.add("count", count);
     if (count > 0 || freq.processEmpty) {
@@ -440,7 +440,7 @@ public abstract class FacetProcessor<FacetRequestT extends FacetRequest>  {
   }
 
   @SuppressWarnings({"unchecked"})
-  void processSubs(SimpleOrderedMap<Object> response, Query filter, DocSet domain, boolean skip, Map<String,Object> facetInfo) throws IOException {
+  protected void processSubs(SimpleOrderedMap<Object> response, Query filter, DocSet domain, boolean skip, Map<String,Object> facetInfo) throws IOException {
 
     boolean emptyDomain = domain == null || domain.size() == 0;
 

cowpaths/solr

solr/core/src/java/org/apache/solr/search/facet/FacetParser.java
@@ -19,6 +19,8 @@ package org.apache.solr.search.facet;
 import static org.apache.solr.common.params.CommonParams.SORT;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -33,7 +35,7 @@ import org.apache.solr.search.FunctionQParser;
 import org.apache.solr.search.QParser;
 import org.apache.solr.search.SyntaxError;
 
-abstract class FacetParser<T extends FacetRequest> {
+public abstract class FacetParser<T extends FacetRequest> {
   protected T facet;
   protected FacetParser<?> parent;
   protected String key;
@@ -58,7 +60,7 @@ abstract class FacetParser<T extends FacetRequest> {
     return new SolrException(SolrException.ErrorCode.BAD_REQUEST, msg + " , path=" + getPathStr());
   }
 
-  public abstract FacetRequest parse(Object o) throws SyntaxError;
+  public abstract T parse(Object o) throws SyntaxError;
 
   // TODO: put the FacetRequest on the parser object?
   public void parseSubs(Object o) throws SyntaxError {
@@ -139,22 +141,16 @@ abstract class FacetParser<T extends FacetRequest> {
   }
 
   public Object parseFacetOrStat(String key, String type, Object args) throws SyntaxError {
-    // TODO: a place to register all these facet types?
-
-    switch (type) {
-      case "field":
-      case "terms":
-        return new FacetFieldParser(this, key).parse(args);
-      case "query":
-        return new FacetQueryParser(this, key).parse(args);
-      case "range":
-        return new FacetRangeParser(this, key).parse(args);
-      case "heatmap":
-        return new FacetHeatmap.Parser(this, key).parse(args);
-      case "func":
-        return parseStat(key, args);
+    // First check facet parser factories.
+    FacetParserFactory facetParserFactory = getSolrRequest().getCore().getFacetParserFactory(type);
+    if (facetParserFactory != null) {
+      return facetParserFactory.create(this, key).parse(args);
     }
-
+    // Then handle special case func directly.
+    if (type.equals("func")) {
+      return parseStat(key, args);
+    }
+    // Otherwise, unknown.
     throw err("Unknown facet or stat. key=" + key + " type=" + type + " args=" + args);
   }
 
@@ -530,6 +526,13 @@ abstract class FacetParser<T extends FacetRequest> {
 
       return facet;
     }
+
+    public static class Factory implements FacetParserFactory {
+      @Override
+      public FacetParser<?> create(FacetParser<?> parent, String key) {
+        return new FacetQueryParser(parent, key);
+      }
+    }
   }
 
   static class FacetFieldParser extends FacetParser<FacetField> {
@@ -670,5 +673,24 @@ abstract class FacetParser<T extends FacetRequest> {
       }
       return facetSort;
     }
+
+    public static class Factory implements FacetParserFactory {
+      @Override
+      public FacetParser<?> create(FacetParser<?> parent, String key) {
+        return new FacetFieldParser(parent, key);
+      }
+    }
+  }
+
+  public static final Map<String, Class<? extends FacetParserFactory>> standard_factories;
+
+  static {
+    HashMap<String, Class<? extends FacetParserFactory>> map = new HashMap<>();
+    map.put("field", FacetParser.FacetFieldParser.Factory.class);
+    map.put("terms", FacetParser.FacetFieldParser.Factory.class);
+    map.put("query", FacetParser.FacetQueryParser.Factory.class);
+    map.put("range", FacetRangeParser.Factory.class);
+    map.put("heatmap", FacetHeatmap.Parser.Factory.class);
+    standard_factories = Collections.unmodifiableMap(map);
   }
 }

solr/core/src/java/org/apache/solr/search/facet/FacetProcessor.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.IntFunction;
+import java.util.stream.Collectors;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -45,19 +46,19 @@ import org.apache.solr.search.facet.SlotAcc.SlotContext;
 
 /** Base abstraction for a class that computes facets. This is fairly internal to the module. */
 public abstract class FacetProcessor<T extends FacetRequest> {
-  SimpleOrderedMap<Object> response;
-  FacetContext fcontext;
+  protected SimpleOrderedMap<Object> response;
+  protected FacetContext fcontext;
   // TODO : I'm not sure this needs to be generic but come back to this later
-  T freq;
+  protected T freq;
 
   // TODO: do these need to be on the context to support recomputing during multi-select?
   DocSet filter; // additional filters specified by "filter"
 
   LinkedHashMap<String, SlotAcc> accMap;
   SlotAcc[] accs;
-  SlotAcc.CountSlotAcc countAcc;
+  protected SlotAcc.CountSlotAcc countAcc;
 
-  FacetProcessor(FacetContext fcontext, T freq) {
+  public FacetProcessor(FacetContext fcontext, T freq) {
     this.fcontext = fcontext;
     this.freq = freq;
     fcontext.processor = this;
@@ -229,8 +230,7 @@ public abstract class FacetProcessor<T extends FacetRequest> {
       }
     }
 
-    // recompute the base domain
-    fcontext.base = fcontext.searcher.getDocSet(qlist);
+    return qlist;
   }
 
   /** modifies the context base if there is a join field domain change */
@@ -338,8 +338,10 @@ public abstract class FacetProcessor<T extends FacetRequest> {
     }
   }
 
-  long collect(DocSet docs, int slot, IntFunction<SlotContext> slotContext) throws IOException {
+  protected long collect(DocSet docs, int slot, IntFunction<SlotContext> slotContext)
+      throws IOException {
     long count = 0;
+
     SolrIndexSearcher searcher = fcontext.searcher;
 
     if (0 == docs.size()) {
@@ -397,8 +399,9 @@ public abstract class FacetProcessor<T extends FacetRequest> {
     }
   }
 
-  void addStats(SimpleOrderedMap<Object> target, int slotNum) throws IOException {
+  protected void addStats(SimpleOrderedMap<Object> target, int slotNum) throws IOException {
     long count = countAcc.getCount(slotNum);
+
     target.add("count", count);
     if (count > 0 || freq.processEmpty) {
       for (SlotAcc acc : accs) {
@@ -452,7 +455,7 @@ public abstract class FacetProcessor<T extends FacetRequest> {
   }
 
   @SuppressWarnings({"unchecked"})
-  void processSubs(
+  protected void processSubs(
       SimpleOrderedMap<Object> response,
       Query filter,
       DocSet domain,
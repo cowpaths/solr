fd62884439827eb55b169ead6bc6d418ae0b6bc1
patsonluk <patson@fullstory.com>
2023-03-14 20:17:32 -0700

    Reverted change to ClusterStatus

--- solr/core/src/java/org/apache/solr/handler/admin/ClusterStatus.java
+++ solr/core/src/java/org/apache/solr/handler/admin/ClusterStatus.java
@@ -189,10 +189,10 @@ public class ClusterStatus {
       }
       String configName = clusterStateCollection.getConfigName();
       collectionStatus.put("configName", configName);
-//      if (message.getBool("prs", false) && clusterStateCollection.isPerReplicaState()) {
-//        PerReplicaStates prs = clusterStateCollection.getPerReplicaStates();
-//        collectionStatus.put("PRS", prs);
-//      } //TODO
+      if (message.getBool("prs", false) && clusterStateCollection.isPerReplicaState()) {
+        PerReplicaStates prs = clusterStateCollection.getPerReplicaStates();
+        collectionStatus.put("PRS", prs);
+      }
       collectionProps.add(name, collectionStatus);
     }
 

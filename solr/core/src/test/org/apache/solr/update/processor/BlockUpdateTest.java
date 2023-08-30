6f39a125f7a444bcb116b6e551b0cb3a66613691
noblepaul <noble.paul@gmail.com>
2023-08-30 19:47:43 +1000

    tidy

--- solr/core/src/test/org/apache/solr/update/processor/BlockUpdateTest.java
+++ solr/core/src/test/org/apache/solr/update/processor/BlockUpdateTest.java
@@ -29,63 +29,63 @@ import org.apache.solr.update.CommitUpdateCommand;
 import org.junit.BeforeClass;
 
 public class BlockUpdateTest extends UpdateProcessorTestBase {
-    public static final String CONFIG_XML = "solrconfig-block-update-processor.xml";
-    public static final String SCHEMA_XML = "schema-block-update.xml";
+  public static final String CONFIG_XML = "solrconfig-block-update-processor.xml";
+  public static final String SCHEMA_XML = "schema-block-update.xml";
 
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        initCore(CONFIG_XML, SCHEMA_XML);
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    initCore(CONFIG_XML, SCHEMA_XML);
+  }
+
+  public void testBulkUpdate() throws Exception {
+    processAdds(
+        "block-update",
+        params("sessionBlock", "true", "commit", "true"),
+        doc(f("Id", "11"), f("desc", "Session 11"), f("Kind", "session"), f("SessionId", 11)),
+        doc(f("Id", "11!1"), f("desc", "Event 11!1"), f("Kind", "event"), f("SessionId", 11)));
+    assertJQ(req("Id:11!1"), "response/docs/[0]/_nest_parent_==\"11\"");
+    processAdds(
+        "block-update",
+        params("sessionBlock", "true", "commit", "true"),
+        doc(f("Id", "11!2"), f("desc", "Event 11!2"), f("Kind", "event"), f("SessionId", 11)),
+        doc(f("Id", "11!3"), f("desc", "Event 11!3"), f("Kind", "event"), f("SessionId", 11)));
+    assertJQ(req("Id:11!1"), "response/docs/[0]/_nest_parent_==\"11\"");
+    assertJQ(req("Id:11!2"), "response/docs/[0]/_nest_parent_==\"11\"");
+    assertJQ(req("Id:11!3"), "response/docs/[0]/_nest_parent_==\"11\"");
+  }
+
+  /**
+   * Runs a document through the specified chain, and returns the final document used when the chain
+   * is completed (NOTE: some chains may modify the document in place
+   */
+  protected void processAdds(
+      final String chain, final SolrParams requestParams, final SolrInputDocument... docs)
+      throws IOException {
+
+    SolrCore core = h.getCore();
+    UpdateRequestProcessorChain pc = core.getUpdateProcessingChain(chain);
+    assertNotNull("No Chain named: " + chain, pc);
+
+    SolrQueryResponse rsp = new SolrQueryResponse();
+
+    SolrQueryRequest req = new LocalSolrQueryRequest(core, requestParams);
+    UpdateRequestProcessor processor = pc.createProcessor(req, rsp);
+
+    try {
+      SolrRequestInfo.setRequestInfo(new SolrRequestInfo(req, rsp));
+      for (SolrInputDocument doc : docs) {
+        AddUpdateCommand cmd = new AddUpdateCommand(req);
+        cmd.solrDoc = doc;
+        processor.processAdd(cmd);
+      }
+      if (requestParams.getBool("commit", false)) {
+        processor.processCommit(new CommitUpdateCommand(req, false));
+      }
+    } finally {
+      processor.finish();
+      processor.close();
+      SolrRequestInfo.clearRequestInfo();
+      req.close();
     }
-
-    public void testBulkUpdate() throws Exception {
-        processAdds(
-                "block-update",
-                params("sessionBlock", "true", "commit", "true"),
-                doc(f("Id", "11"), f("desc", "Session 11"), f("Kind", "session"), f("SessionId", 11)),
-                doc(f("Id", "11!1"), f("desc", "Event 11!1"), f("Kind", "event"), f("SessionId", 11)));
-        assertJQ(req("Id:11!1"), "response/docs/[0]/_nest_parent_==\"11\"");
-        processAdds(
-                "block-update",
-                params("sessionBlock", "true", "commit", "true"),
-                doc(f("Id", "11!2"), f("desc", "Event 11!2"), f("Kind", "event"), f("SessionId", 11)),
-                doc(f("Id", "11!3"), f("desc", "Event 11!3"), f("Kind", "event"), f("SessionId", 11)));
-        assertJQ(req("Id:11!1"), "response/docs/[0]/_nest_parent_==\"11\"");
-        assertJQ(req("Id:11!2"), "response/docs/[0]/_nest_parent_==\"11\"");
-        assertJQ(req("Id:11!3"), "response/docs/[0]/_nest_parent_==\"11\"");
-    }
-
-    /**
-     * Runs a document through the specified chain, and returns the final document used when the chain
-     * is completed (NOTE: some chains may modify the document in place
-     */
-    protected void processAdds(
-            final String chain, final SolrParams requestParams, final SolrInputDocument... docs)
-            throws IOException {
-
-        SolrCore core = h.getCore();
-        UpdateRequestProcessorChain pc = core.getUpdateProcessingChain(chain);
-        assertNotNull("No Chain named: " + chain, pc);
-
-        SolrQueryResponse rsp = new SolrQueryResponse();
-
-        SolrQueryRequest req = new LocalSolrQueryRequest(core, requestParams);
-        UpdateRequestProcessor processor = pc.createProcessor(req, rsp);
-
-        try {
-            SolrRequestInfo.setRequestInfo(new SolrRequestInfo(req, rsp));
-            for (SolrInputDocument doc : docs) {
-                AddUpdateCommand cmd = new AddUpdateCommand(req);
-                cmd.solrDoc = doc;
-                processor.processAdd(cmd);
-            }
-            if (requestParams.getBool("commit", false)) {
-                processor.processCommit(new CommitUpdateCommand(req, false));
-            }
-        } finally {
-            processor.finish();
-            processor.close();
-            SolrRequestInfo.clearRequestInfo();
-            req.close();
-        }
-    }
+  }
 }

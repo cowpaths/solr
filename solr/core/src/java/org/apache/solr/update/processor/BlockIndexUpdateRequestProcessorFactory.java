d30d22afb779c167691ae6931ea304aa916f1486
noblepaul <noble.paul@gmail.com>
2023-08-30 19:47:43 +1000

    tidy

--- solr/core/src/java/org/apache/solr/update/processor/BlockIndexUpdateRequestProcessorFactory.java
+++ solr/core/src/java/org/apache/solr/update/processor/BlockIndexUpdateRequestProcessorFactory.java
@@ -34,109 +34,108 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BlockIndexUpdateRequestProcessorFactory extends UpdateRequestProcessorFactory {
-    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+  @Override
+  public UpdateRequestProcessor getInstance(
+      SolrQueryRequest req, SolrQueryResponse rsp, UpdateRequestProcessor next) {
+    return new URP(req, rsp, next);
+  }
+
+  private class URP extends UpdateRequestProcessor {
+    final boolean isEnabled;
+    private Map<String, SolrInputDocument> sessions = new LinkedHashMap<>();
+    private Map<String, List<SolrInputDocument>> events = new LinkedHashMap<>();
+    private final SolrQueryRequest req;
+
+    public URP(SolrQueryRequest req, SolrQueryResponse rsp, UpdateRequestProcessor next) {
+      super(next);
+      this.req = req;
+      isEnabled = true;
+    }
 
     @Override
-    public UpdateRequestProcessor getInstance(
-            SolrQueryRequest req, SolrQueryResponse rsp, UpdateRequestProcessor next) {
-        return new URP(req, rsp, next);
+    public void processAdd(AddUpdateCommand cmd) throws IOException {
+      if (!isEnabled) {
+        super.processAdd(cmd);
+        return;
+      }
+      if (cmd.solrDoc != null) {
+        String kind = (String) cmd.solrDoc.getFieldValue("Kind");
+        if ("session".equals(kind)) {
+          sessions.put(String.valueOf(cmd.solrDoc.getFieldValue("SessionId")), cmd.solrDoc);
+          return;
+        }
+        if ("event".equals(kind)) {
+          String session = String.valueOf(cmd.solrDoc.getFieldValue("SessionId"));
+          if (session != null) {
+            SolrInputDocument s = sessions.get(session);
+            if (s != null) {
+              s.addField("events", cmd.solrDoc);
+            } else {
+              events.computeIfAbsent(session, s1 -> new ArrayList<>()).add(cmd.solrDoc);
+            }
+          } else {
+            // no sessionId in the event. It shouldn't happen
+            super.processAdd(cmd);
+          }
+        } else super.processAdd(cmd);
+      }
     }
 
-    private class URP extends UpdateRequestProcessor {
-        final boolean isEnabled;
-        private Map<String, SolrInputDocument> sessions = new LinkedHashMap<>();
-        private Map<String, List<SolrInputDocument>> events = new LinkedHashMap<>();
-        private final SolrQueryRequest req;
-
-        public URP(SolrQueryRequest req, SolrQueryResponse rsp, UpdateRequestProcessor next) {
-            super(next);
-            this.req = req;
-            isEnabled = true;
-        }
-
-        @Override
-        public void processAdd(AddUpdateCommand cmd) throws IOException {
-            if (!isEnabled) {
-                super.processAdd(cmd);
-                return;
-            }
-            if (cmd.solrDoc != null) {
-                String kind = (String) cmd.solrDoc.getFieldValue("Kind");
-                if ("session".equals(kind)) {
-                    sessions.put(String.valueOf(cmd.solrDoc.getFieldValue("SessionId")), cmd.solrDoc);
-                    return;
-                }
-                if ("event".equals(kind)) {
-                    String session = String.valueOf(cmd.solrDoc.getFieldValue("SessionId"));
-                    if (session != null) {
-                        SolrInputDocument s = sessions.get(session);
-                        if (s != null) {
-                            s.addField("events", cmd.solrDoc);
-                        } else {
-                            events.computeIfAbsent(session, s1 -> new ArrayList<>()).add(cmd.solrDoc);
-                        }
-                    } else {
-                        // no sessionId in the event. It shouldn't happen
-                        super.processAdd(cmd);
-                    }
-                } else super.processAdd(cmd);
-            }
-        }
-
-        @Override
-        public void processCommit(CommitUpdateCommand cmd) throws IOException {
-            processNested();
-            super.processCommit(cmd);
-        }
-
-        @Override
-        public void finish() throws IOException {
-            if (!isEnabled) {
-                super.finish();
-                return;
-            }
-            processNested();
-            next.finish();
-        }
-
-        private void processNested() throws IOException {
-            if (!sessions.isEmpty()) {
-                for (SolrInputDocument d : sessions.values()) {
-                    AddUpdateCommand add = new AddUpdateCommand(req);
-                    add.solrDoc = d;
-                    next.processAdd(add);
-                }
-                sessions.clear();
-            }
-            if (!events.isEmpty()) {
-                for (Map.Entry<String, List<SolrInputDocument>> e : events.entrySet()) {
-                    BytesRef session = new BytesRef(e.getKey());
-                    final SolrInputDocument oldSessionWithoutChildren =
-                            RealTimeGetComponent.getInputDocument(
-                                    req.getCore(),
-                                    session,
-                                    session,
-                                    null,
-                                    null,
-                                    RealTimeGetComponent.Resolution.DOC); // when no children, just fetches the doc
-                    if (oldSessionWithoutChildren == null) {
-                        // strange. The session does not exist. just index as is
-                        for (SolrInputDocument d : e.getValue()) {
-                            AddUpdateCommand add = new AddUpdateCommand(req);
-                            add.solrDoc = d;
-                            next.processAdd(add);
-                        }
-
-                    } else {
-                        oldSessionWithoutChildren.addField("events", Map.of("add", e.getValue()));
-                        AddUpdateCommand add = new AddUpdateCommand(req);
-                        add.solrDoc = oldSessionWithoutChildren;
-                        next.processAdd(add);
-                    }
-                }
-                events.clear();
-            }
-        }
+    @Override
+    public void processCommit(CommitUpdateCommand cmd) throws IOException {
+      processNested();
+      super.processCommit(cmd);
     }
+
+    @Override
+    public void finish() throws IOException {
+      if (!isEnabled) {
+        super.finish();
+        return;
+      }
+      processNested();
+      next.finish();
+    }
+
+    private void processNested() throws IOException {
+      if (!sessions.isEmpty()) {
+        for (SolrInputDocument d : sessions.values()) {
+          AddUpdateCommand add = new AddUpdateCommand(req);
+          add.solrDoc = d;
+          next.processAdd(add);
+        }
+        sessions.clear();
+      }
+      if (!events.isEmpty()) {
+        for (Map.Entry<String, List<SolrInputDocument>> e : events.entrySet()) {
+          BytesRef session = new BytesRef(e.getKey());
+          final SolrInputDocument oldSessionWithoutChildren =
+              RealTimeGetComponent.getInputDocument(
+                  req.getCore(),
+                  session,
+                  session,
+                  null,
+                  null,
+                  RealTimeGetComponent.Resolution.DOC); // when no children, just fetches the doc
+          if (oldSessionWithoutChildren == null) {
+            // strange. The session does not exist. just index as is
+            for (SolrInputDocument d : e.getValue()) {
+              AddUpdateCommand add = new AddUpdateCommand(req);
+              add.solrDoc = d;
+              next.processAdd(add);
+            }
+
+          } else {
+            oldSessionWithoutChildren.addField("events", Map.of("add", e.getValue()));
+            AddUpdateCommand add = new AddUpdateCommand(req);
+            add.solrDoc = oldSessionWithoutChildren;
+            next.processAdd(add);
+          }
+        }
+        events.clear();
+      }
+    }
+  }
 }

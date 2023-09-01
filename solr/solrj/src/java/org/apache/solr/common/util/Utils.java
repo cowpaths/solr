3f81865c62f54b1d97c2808b6c0e770ac0981e8a
Justin Sweeney <justinsweeney@fullstory.com>
2023-09-01 04:34:20 -0600

    Re-adding 0 indentation of doc colleciton JSON and fixing test (#131)

--- solr/solrj/src/java/org/apache/solr/common/util/Utils.java
+++ solr/solrj/src/java/org/apache/solr/common/util/Utils.java
@@ -288,14 +288,7 @@ public class Utils {
   public static byte[] toJSON(Object o) {
     if (o == null) return new byte[0];
     CharArr out = new CharArr();
-    //    if (!(o instanceof List) && !(o instanceof Map)) {
-    //      if (o instanceof MapWriter) {
-    //        o = ((MapWriter) o).toMap(new LinkedHashMap<>());
-    //      } else if (o instanceof IteratorWriter) {
-    //        o = ((IteratorWriter) o).toList(new ArrayList<>());
-    //      }
-    //    }
-    new MapWriterJSONWriter(out, 0).write(o); // indentation by default
+    new JSONWriter(out, 0).write(o); // indentation by default
     return toUTF8(out);
   }
 

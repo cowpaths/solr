fc5120a92b1e62cf4dd28c592409c38eca370cf0
noblepaul <noble.paul@gmail.com>
2024-02-13 04:36:31 +1100

    Use a System wide property to enable PRS. the testcase can piggyback on that property to randomize PRS

--- solr/core/src/test/org/apache/solr/cloud/api/collections/SimpleCollectionCreateDeleteTest.java
+++ solr/core/src/test/org/apache/solr/cloud/api/collections/SimpleCollectionCreateDeleteTest.java
@@ -28,7 +28,6 @@ import org.apache.solr.cloud.OverseerCollectionConfigSetProcessor;
 import org.apache.solr.common.cloud.ClusterState;
 import org.apache.solr.common.cloud.DocCollection;
 import org.apache.solr.common.cloud.PerReplicaStatesFetcher;
-import org.apache.solr.common.cloud.PerReplicaStatesOps;
 import org.apache.solr.common.cloud.SolrZkClient;
 import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.util.NamedList;

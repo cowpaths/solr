a1582f65e66884476761040fe43612c08b79c9c0
patsonluk <patson@fullstory.com>
2024-04-17 18:26:20 -0700

    javadoc

--- solr/core/src/java/org/apache/solr/core/SyntheticSolrCore.java
+++ solr/core/src/java/org/apache/solr/core/SyntheticSolrCore.java
@@ -8,6 +8,14 @@ import org.apache.solr.common.SolrException;
 import org.apache.solr.common.params.CoreAdminParams;
 import org.apache.solr.rest.RestManager;
 
+/**
+ * A synthetic core that is created only in memory and not registered against Zookeeper.
+ *
+ * <p>This is only used in Coordinator node to support a subset of SolrCore functionalities required by Coordinator
+ * operations such as aggregating and writing out response and providing configset info.
+ *
+ * <p>There should only be one instance of SyntheticSolrCore per configset
+ */
 public class SyntheticSolrCore extends SolrCore {
   public SyntheticSolrCore(CoreContainer coreContainer, CoreDescriptor cd, ConfigSet configSet) {
     super(coreContainer, cd, configSet);
@@ -39,11 +47,14 @@ public class SyntheticSolrCore extends SolrCore {
 
   @Override
   protected void bufferUpdatesIfConstructing(CoreDescriptor coreDescriptor) {
-    //no updates to SyntheticSolrCore
+    // no updates to SyntheticSolrCore
   }
 
   @Override
   protected RestManager initRestManager() throws SolrException {
-    return new RestManager(); //TODO explain why we cannot use the super class init routine
+    // returns an initialized RestManager. As init routines requires reading configname of the core's collection from ZK
+    // which synthetic core is not registered in ZK.
+    // We do not expect RestManager ops on Coordinator Nodes
+    return new RestManager();
   }
 }

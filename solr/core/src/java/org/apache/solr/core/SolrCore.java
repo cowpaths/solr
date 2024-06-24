2da8c9ef37f2e068b747681cbe932a7ac4fbf5ea
patsonluk <patson@fullstory.com>
2024-06-24 15:54:28 -0700

    Undo changes to SolrCore#postDecorateResponse and add the json log to RequestHandlerBase instead. This gives better ordering on the log message contents

--- solr/core/src/java/org/apache/solr/core/SolrCore.java
+++ solr/core/src/java/org/apache/solr/core/SolrCore.java
@@ -16,8 +16,6 @@
  */
 package org.apache.solr.core;
 
-import static org.apache.solr.common.params.CommonParams.DISTRIB;
-import static org.apache.solr.common.params.CommonParams.JSON;
 import static org.apache.solr.common.params.CommonParams.PATH;
 
 import com.codahale.metrics.Counter;
@@ -2991,13 +2989,6 @@ public class SolrCore implements SolrInfoBean, Closeable {
     // TODO should check that responseHeader has not been replaced by handler
     NamedList<Object> responseHeader = rsp.getResponseHeader();
     if (responseHeader == null) return;
-
-    SolrParams params = req.getParams();
-    if (params.getParams(JSON) != null && params.getBool(DISTRIB, true)) {
-      //log JSON for top level query, this includes query embedded in POST query
-      rsp.getToLog().add("json", String.join(" ", params.getParams(JSON)));
-    }
-
     final int qtime = (int) (req.getRequestTimer().getTime());
     int status = 0;
     Exception exception = rsp.getException();

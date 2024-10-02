ad302eefb90340b51769155d4f5914a31262078d
patsonluk <patsonluk@users.noreply.github.com>
2024-10-02 09:29:56 -0700

    SAI-5048: Some enhancements for node level metrics (#220)
    * Use aggregate metrics where possible
    
    * typo
    
    * Added support for all attributes
    
    * Added support cumulative adds and errors
    
    * Added test for cumulative adds and errors
    
    * refactored to eliminate hard coing keys
    
    * added all other available metrics
    
    * refactor
    
    * 1. Added more explicit dependency between AggregateMetricsApiCaller and CoresMetricsApiCaller
    2. CoresMetricsApiCaller will only fetch metrics that are missing from AggregateMetricsApiCaller
    3. Added capability to get property other than counter (for median duration etc) - Added support of key and expr on top of prefix/property for metrics api querying
    4. Some fixes to label and unit test cases
    5. Added metricType to CoreMetric to retain correct Prometheus metrics type
    
    * Fixed gradle check error
    
    * Added p50 p95 and p99 metrics for various ops
    
    * Fixed test case issue
    
    * ./gradlew tidy
    
    * Fixed local update metrics
    
    * Temporarily disabled the CoresMetricsApiCaller
    
    * Do not call metricx expr, as it's costly
    
    * Do not call metricx expr, as it's costly
    
    * Simplified aggregated metric fetching logic (#221)
    
    * Combine both core and solr.node metrics into AggregateMetricsApiCaller to speed things up
    
    * Fixed metrics api property query param
    
    * Fixed metrics api property query param
    
    * Avoid NPE
    
    * javadoc
    code cleanup
    re-arranged CoreMetric enum ordering to fit the test case
    
    * Undo unnecessary change
    
    * Fixed a logic error
    
    * ./gradlew tidy
    
    * Use 2 prometheus metrics calls (#222)
    
    * Try making 2 calls and see if it's faster
    
    * Try making 2 calls and see if it's faster
    
    * Fixed unit test cases
    
    * ./gradlew tidy
    
    * Use meter for autocommit metrics
    
    * Adjust PrometheusMetricsServlet after changing to meter for autocommit metrics
    
    * ./gradlew tidy and fixing unit test cases
    
    ---------
    
    Co-authored-by: noblepaul <noble.paul@gmail.com>

--- solr/core/src/java/org/apache/solr/update/CommitUpdateCommand.java
+++ solr/core/src/java/org/apache/solr/update/CommitUpdateCommand.java
@@ -27,6 +27,8 @@ public class CommitUpdateCommand extends UpdateCommand {
   public boolean expungeDeletes = false;
   public boolean softCommit = false;
   public boolean prepareCommit = false;
+  boolean autoCommit = false;
+
   /**
    * User provided commit data. Can be let to null if there is none. It is possible to commit this
    * user data, even if there is no uncommitted change in the index writer, provided this user data
